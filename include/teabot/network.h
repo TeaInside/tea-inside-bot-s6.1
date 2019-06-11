46c0f3b7e614ec26d2410bcb330a8973c22b9eae
Ammar Faizi <ammarfaizi2@gmail.com>
2019-06-11 23:20:25 +0700

    Prepare network and config example

---
+++ include/teabot/network.h
@@ -0,0 +1,7 @@
+
+#ifndef teabot__network
+#define teabot__network
+
+char * send_message();
+
+#endif
---
+++ src/teabot/network/network.c
@@ -0,0 +1,7 @@
+
+#include <teabot/network.h>
+
+
+#include <teabot/network.h>
+
+#include "components/send_message.h"
